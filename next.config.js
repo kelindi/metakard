82a0581b89df87b60efad8988ff4d2c6ecd9cb5b
Kelindi <ayushk2020@gmail.com>
2022-02-06 01:49:45 -0500

    nft candymachine

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+},
   reactStrictMode: true,
 }
--- pages/api/createWallet.js
+++ pages/api/createWallet.js
@@ -1,9 +1,19 @@
 import { execOnce } from "next/dist/shared/lib/utils"
+import fs from 'fs'
+import path from 'path'
 const { exec } = require('child_process');
-// Fake users data
-const users = [{ id: 1 }, { id: 2 }, { id: 3 }]
+import getConfig from 'next/config'
 
-export default function handler(req, res) {
-  let a = exec('solana-keygen new --outfile ~/.config/solana/devnet.json  ')
-  res.status(200).json(a)
+// export default function handler(req, res) {
+//   let a = exec('ts-node ./metaplex/js/packages/cli/src/candy-machine-v2-cli.ts upload -e devnet -k ./solana/devnet.json -cp ./configs/config.json -c kard  ./assets')
+  
+//   res.status(200).json(a)
+// }
+
+
+export default (req, res) => {
+  const { serverRuntimeConfig } = getConfig()
+  let file = fs.readFileSync(path.join(serverRuntimeConfig.PROJECT_ROOT,'./.cache/devnet-example.json'))
+  let data = JSON.parse(file)
+  res.status(200).json(data)
 }
